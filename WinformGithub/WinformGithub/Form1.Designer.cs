c103f67dbd33dec167ffdbdc3d9d7c17cd733724
nguyenhoanganhvu <nguyenhoanganhvu.it@gmail.com>
2020-12-30 21:46:26 +0700

    first commit

---
+++ WinformGithub/WinformGithub/Form1.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace WinformGithub
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 268);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Login";
+            this.Text = "Đăng nhập";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
